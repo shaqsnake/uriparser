f748ba6ce78d21a1ec936500b885f7c5f554dbd8
shaqsnake <shaqsnake@gmail.com>
2019-07-25 14:24:38 +0800

    Issue #9. Initial UriPctCoder unittest.

---
+++ test/src/UriPctCoderTests.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2019 shaqsnake. All rights reserved.
+/**
+ * @Author: shaqsnake
+ * @Email: shaqsnake@gmail.com
+ * @Date: 2019-07-25 14:13:54
+ * @LastEditTime: 2019-07-25 14:18:46
+ * @Description: 
+ */
+#include <gtest/gtest.h>
+#include <src/UriPctCoder.hpp>
+
+TEST(UriPctCoderTests, PlaceHolder) {
+    ASSERT_TRUE(true);
+}
